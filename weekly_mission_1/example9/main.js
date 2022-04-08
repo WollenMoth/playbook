d45a0f4566c2e522d2af21ef909fc6f8089e1783
André Díaz <andre290800@gmail.com>
2022-04-07 20:57:03 -0500

    Ejercicio

---
+++ weekly_mission_1/example9/main.js
@@ -0,0 +1,18 @@
+const Pokemon = require("./pokemon");
+
+const pikachu = new Pokemon("pikachu");
+const bulbasaur = new Pokemon("bulbasaur");
+const squirtle = new Pokemon("squirtle");
+const charmander = new Pokemon("charmander");
+
+pikachu.sayHello();
+pikachu.sayMessage("Hey!");
+
+bulbasaur.sayHello();
+bulbasaur.sayMessage("Hey!");
+
+charmander.sayHello();
+charmander.sayMessage("Hey!");
+
+squirtle.sayHello();
+squirtle.sayMessage("Hey!");
